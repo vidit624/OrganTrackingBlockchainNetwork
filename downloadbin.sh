69c7f853e522cc89a973bb6aa7a5006d90a4c749
vidit624 <gpta.vidit1234@iitkgp.ac.in>
2020-12-16 19:23:24 +0530

    Updated the path to download binaries

--- downloadbin.sh
+++ downloadbin.sh
@@ -5,7 +5,7 @@ export VERSION=1.4.0
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 
 echo "===> Downloading platform binaries"
-export URL="https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz"
+export URL="https://github.com/hyperledger/fabric/releases/download/v${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz"
 echo $URL
-curl  $URL| tar xz
+curl  -L $URL| tar xz
 
